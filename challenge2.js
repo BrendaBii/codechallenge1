45b1391c376139adc6a53937555c2c64596bc46b
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-18 13:47:03 +0300

    Completed challenge2.

--- challenge2.js
+++ challenge2.js
@@ -1,19 +1,19 @@
 function myFunction() {
     let speed = prompt("Please enter the speed :", "0");
     let demerits = false;
-    if (speed < 70){
+    if (speed < 70 && speed >= 0){
         document.getElementById("demo").innerHTML = "Ok";
     }
-    else{
-    for (demerits = 0, demerits < 12, demerits++){
-
-    }}
-    
-    if(typeof grade === 'string'){    
-    document.getElementById("demo").innerHTML =
-    "Demerit points : " + demerits;
+    else if(speed >= 70){
+        demerits = ((speed-70)/5);
+        if(demerits > 12){
+            document.getElementById("demo").innerHTML = "License suspended"
+        }
+        else{
+            document.getElementById("demo").innerHTML = "Points : " + Math.floor(demerits);
+        }
     }
-    else if(typeof grade === 'boolean'){
+    else if(typeof demerits === 'boolean'){
     document.getElementById("demo").innerHTML = "Your entry is invalid!";
     }
 }
